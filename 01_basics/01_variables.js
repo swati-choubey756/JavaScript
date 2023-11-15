ba0a1f633cda909c5dedd9d9e808c46df59031e0
swati-choubey756 <150902256+swati-choubey756@users.noreply.github.com>
2023-11-15 11:28:19 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="swati@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId=2   // not allowed
+
+accountEmail="S@gmail.com"
+accountPassword="23141"
+accountCity="Raipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
