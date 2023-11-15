7cd8b4f37ebf37da5122a1501543f5d79d0e1ca7
swati-choubey756 <150902256+swati-choubey756@users.noreply.github.com>
2023-11-15 16:52:33 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,6 +6,8 @@ let accountState;
 
 // accountId=2   // not allowed
 
+//prefer not to use var because of issue in block scope and functional scope
+
 accountEmail="S@gmail.com"
 accountPassword="23141"
 accountCity="Raipur"
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all JS code as newer version
+
+//alert(3+3)// we are using node.js, not browser
+
+let name="swati" //string
+let age=18 //number
+let isLoggedIn=true //boolean
+
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => values is not assigned
+//symbol =>unique
+
+//Object
+
+console.log(typeof "swati")
+console.log(typeof age)
+console.log(typeof null)  //undefined
+console.log(typeof undefined)  //object
